1eac699d873ecc45cdfd527eb3997fa0de32b3db
elvece <lvcifferello@gmail.com>
2019-03-19 20:08:14 -0600

    model setup

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,33 @@
 import * as express from 'express'
 import * as cors from 'cors'
-import * as bodyParser from 'body-parser'
+import * as bp from 'body-parser'
+import { getManager } from 'typeorm'
+import { Rsvp } from './model'
 
 const app: express.Application = express()
 const router: express.Router = express.Router()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('dev'))
+app.use(bp.json({ type: 'application/json' }))
+app.use(bp.urlencoded({ type: 'application/x-www-form-urlencoded', extended: false }))
 app.use(cors())
 
-app.use('/', router)
+app.use('/register', async (req, res, next) => {
+  const data = req.body as RegisterRequest
+  try {
+    const rsvp = await getManager().create(Rsvp, { ...data })
+    res.status(200).json(rsvp)
+  } catch (e) {
+    res.status(500).json(e)
+  }
+})
 
 export default app
+
+interface RegisterRequest {
+  firstName: string
+  lastName: string
+  email: string
+  reply: boolean
+}
+
---
+++ src/model.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryGeneratedColumn, Column, UpdateDateColumn } from 'typeorm'
+
+@Entity({ name: 'rsvp' })
+export class Rsvp {
+
+  newtype: 'Rsvp'
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column('timestamp', { name: 'created_at', default: () => 'now()' })
+  createdAt: Date
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date
+
+  @Column('text', { name: 'first_name' })
+  firstName: string
+
+  @Column('text', { name: 'last_name' })
+  lastName: string
+
+  @Column('text', { name: 'email' })
+  email: string
+
+  @Column('boolean', { name: 'reply' })
+  reply: boolean
+}
