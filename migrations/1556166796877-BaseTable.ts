45f387498e5ef074e639649f60f927f00544a158
elvece <lvcifferello@gmail.com>
2019-04-24 23:57:48 -0600

    base table migration

---
+++ migrations/1556166796877-BaseTable.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class BaseTable1556166796877 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`CREATE TABLE "rsvp" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "first_name" text NOT NULL, "last_name" text NOT NULL, "email" text NOT NULL, "reply" boolean NOT NULL, CONSTRAINT "PK_33487519e664b4559d391ab71fb" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query(`DROP TABLE "rsvp"`);
+    }
+
+}
