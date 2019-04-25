c44d658f18c21bb27901cba7231232300f325bce
elvece <lvcifferello@gmail.com>
2019-04-24 23:57:30 -0600

    validation and routes working

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,18 @@ import * as bp from 'body-parser'
 import { getManager } from 'typeorm'
 import { Rsvp } from './model'
 
+import * as Ajv from 'ajv'
+var ajv = new Ajv({ allErrors: true })
+
+var schema = {
+  type: 'object',
+  properties: {
+    firstName: { type: 'string', pattern: '^(?!.*\\d)', minLength: 2, maxLength: 120 },
+    lastName: { type: 'string', pattern: '^(?!.*\\d)', minLength: 2, maxLength: 120 },
+    email: { type: 'string', format: 'email', maxLength: 128 }
+  }
+}
+
 const app: express.Application = express()
 const router: express.Router = express.Router()
 
@@ -13,11 +25,10 @@ app.use(bp.json({ type: 'application/json' }))
 app.use(bp.urlencoded({ type: 'application/x-www-form-urlencoded', extended: false }))
 app.use(cors())
 
-app.post('/rsvp/register', async (req, res, next) => {
-  // todo validate
+app.post('/rsvp/register', validate(ajv.compile(schema)), async (req, res, next) => {
   const data = req.body as RegisterRequest
   try {
-    const rsvp = await getManager().create(Rsvp, { ...data })
+    const rsvp = await getManager().save(Rsvp, { ...data })
     res.status(200).json(rsvp)
   } catch (e) {
     res.status(500).json(e)
@@ -27,7 +38,6 @@ app.post('/rsvp/register', async (req, res, next) => {
 
 app.get('/rsvp/all', async (req, res, next) => {
   // todo auth me
-  const data = req.body as RegisterRequest
   try {
     const all = await getManager().find(Rsvp)
     res.status(200).json(all)
@@ -46,3 +56,18 @@ interface RegisterRequest {
   reply: boolean
 }
 
+
+export function validate(validator: Ajv.ValidateFunction) {
+  return function (req, res, next) {
+    if (validator(req.body) !== true) {
+      next(JSON.stringify(validator.errors))
+    } else {
+      next()
+    }
+  }
+}
+
+// catch all error handling
+app.use(function (err, req, res, next) {
+  res.status(500).send(err)
+})
