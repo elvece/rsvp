1e7025f39a866bb885233c0403aed7bd482a7bfd
elvece <lvcifferello@gmail.com>
2019-06-02 10:56:34 -0600

    table styled

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -7,7 +7,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Routes />
+        <div className="app-content">
+          <Routes />
+        </div>
         <Footer/>
       </div>
     )
--- client/src/app/routes/RsvpTable/RsvpTable.tsx
+++ client/src/app/routes/RsvpTable/RsvpTable.tsx
@@ -18,29 +18,29 @@ class RsvpTable extends Component {
     return (
         <div className="RsvpTable">
           <table className="RsvpTable-table">
-            <thead className="">
-              <tr className="">
-                <th colSpan={1} className="">
-                  Name
+          <thead>
+            <tr className="RsvpTable-row-header">
+                <th colSpan={1}>
+                  name
                 </th>
-                <th colSpan={2} className="">
-                  Reply
+                <th colSpan={2}>
+                  reply
                 </th>
-                <th colSpan={1} className="">
-                  Note
+                <th colSpan={1}>
+                  note
                 </th>
               </tr>
             </thead>
-            <tbody>
+            <tbody className="RsvpTable-body">
               {this.state.responses.map(response => (
-                <tr className="" key={response.id}>
-                  <td colSpan={1} className="">
+                <tr key={response.id}>
+                  <td colSpan={1}>
                       {response.firstName} {response.lastName}
                   </td>
-                  <td colSpan={1} className="">
+                  <td colSpan={1}>
                       {response.reply ? 'yes' : 'no'}
                   </td>
-                  <td colSpan={1} className="">
+                  <td colSpan={2}>
                       {response.note}
                   </td>
                 </tr>
